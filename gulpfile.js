f8d211a5c618b8f19bcb7338ae312018498aa953
user <user@213.180.221.72-ekb.dhcp.yndx.net>
2014-11-09 14:30:35 +0500

    gulp - stylus

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 // var watcher = require('gulp-watcher');
 // var min = require('gulp-min');
-// var stylus = require('gulp-stylus');
+var stylus = require('gulp-stylus');
 // var copy = require('gulp-copy');
 // var jshint = require('gulp-jshint');
 // var imagemin = require('gulp-imagemin');
@@ -11,23 +11,29 @@ var concat = require('gulp-concat');
 // var imagemin = require('gulp-imagemin');
 
 
-gulp.task('scripts', function() {
-  gulp.src('./blocks/*.js')
+gulp.task('concat-scripts', function() {
+  gulp.src('./blocks/**/*.js')
     .pipe(concat('index.js'))
     .pipe(gulp.dest('./desktop.bundles/index/'))
 });
 
-gulp.task('styles', function() {
-  gulp.src('./blocks/*.css')
-    .pipe(concat('index.css'))
+gulp.task('concat-stylus', function() {
+  gulp.src('./blocks/**/*.styl')
+    .pipe(concat('index.styl'))
     .pipe(gulp.dest('./desktop.bundles/index/'))
 });
 
-gulp.task('concat',['scripts', 'styles']);
+gulp.task('stylus-generate', function () {
+  gulp.src('./desktop.bundles/index.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./desktop.bundles/index.index.css'));
+});
 
 
+gulp.task('concat',['concat-scripts', 'concat-stylus']);
+gulp.task('stylus',['stylus-generate']);
 
 
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['concat']);
+gulp.task('default', ['concat','stylus']);
