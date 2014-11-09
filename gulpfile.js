ec9b2bf2cfb2f871a8f6a362ba2e5748f26946b7
Creego <user@213.180.221.72-ekb.dhcp.yndx.net>
2014-11-09 18:26:20 +0500

    прикрутил gulp: stylus + concat + prefixer + watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,64 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-// var watcher = require('gulp-watcher');
-// var min = require('gulp-min');
-// var stylus = require('gulp-stylus');
-// var copy = require('gulp-copy');
-// var jshint = require('gulp-jshint');
-// var imagemin = require('gulp-imagemin');
-// var autoprefixer = require('gulp-autoprefixer');
+var stylus = require('gulp-stylus');
+var jshint = require('gulp-jshint');
+var watch = require('gulp-watch');
 // var csscomb = require('gulp-csscomb');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+// var min = require('gulp-min');
+// var copy = require('gulp-copy');
+// var imagemin = require('gulp-imagemin');
+
 // var imagemin = require('gulp-imagemin');
 
 
-gulp.task('scripts', function() {
-  gulp.src('./blocks/*.js')
+gulp.task('concat-scripts', function() {
+  gulp.src('./blocks/**/*.js')
     .pipe(concat('index.js'))
     .pipe(gulp.dest('./desktop.bundles/index/'))
 });
 
-gulp.task('styles', function() {
-  gulp.src('./blocks/*.css')
-    .pipe(concat('index.css'))
-    .pipe(gulp.dest('./desktop.bundles/index/'))
+gulp.task('concat-stylus', function() {
+  gulp.src('./blocks/**/*.styl')
+    .pipe(concat('index.styl'))
+    .pipe(stylus())
+    .pipe(autoprefixer({
+        browsers: ['> 0%'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('./desktop.bundles/index/'));
 });
 
-gulp.task('concat',['scripts', 'styles']);
+gulp.task('lint', function() {
+  return gulp.src('./blocks/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./blocks/**/*.styl', function(event) { 
+	    gulp.run('concat-stylus'); 
+	}); 
+    gulp.watch('./blocks/**/*.js' , function (event) {
+        gulp.run('concat-scripts');
+    });
+});
+
+// gulp.task('comb', function () {
+//   return gulp.src('./blocks/**/*.styl')
+//     .pipe(csscomb())
+//     .pipe(gulp.dest('./blocks/'));
+// });
+
+
+gulp.task('styles', ['concat-stylus']);
+gulp.task('scripts', ['concat-scripts']);
+gulp.task('jshint', ['lint']);
+
+
+gulp.task('default', ['styles','scripts','jshint','watch']);
 
 
 
-
-
-// The default task (called when you run `gulp` from cli)
-gulp.task('default', ['concat']);
